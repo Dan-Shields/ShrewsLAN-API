a05e2bc2cef2c79f9dba40fba2f20529f04808f6
Dan Shields <d.shieldsuk@gmail.com>
2019-06-27 02:46:39 +0100

    feat: add migration and model for Signups

---
+++ app/Signup.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Signup extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'days',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+}
